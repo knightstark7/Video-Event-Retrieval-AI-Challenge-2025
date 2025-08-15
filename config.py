b463f726c3c34d737046631b27c63d75a6fe58bd
ImPhatDat <phatdatn2003@gmail.com>
2025-08-15 07:47:45 +0000

    update config

---
+++ config.py
@@ -0,0 +1,5 @@
+
+MEDIA_DIR = '/mnt/d/AI Challenge/Data/media-info'
+DOWNLOAD_DIR = '/mnt/d/AI Challenge/Data/video'
+SCENE_DIR = '/mnt/d/AI Challenge/Data/scene'
+KEYFRAME_DIR = "/mnt/d/AI Challenge/Data/keyframes"
