2eecca23acb8fd4604e6c56f187bc1495f05fa28
ImPhatDat <phatdatn2003@gmail.com>
2025-08-23 15:11:07 +0000

    add cfg

--- cfg.py
+++ cfg.py
@@ -3,3 +3,5 @@ MEDIA_DIR = '/mnt/d/AI Challenge/Data/media-info'
 DOWNLOAD_DIR = '/mnt/d/AI Challenge/Data/video'
 SCENE_DIR = '/mnt/d/AI Challenge/Data/scene'
 KEYFRAME_DIR = "/mnt/d/AI Challenge/Data/keyframes"
+FEATURE_DIR = "/mnt/d/AI Challenge/Data/features"
+ZIP_PATH = f"{FEATURE_DIR}/features.zip"
