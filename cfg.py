19fca715fd22476e8c3ce18b9248dd3788e5828d
ImPhatDat <phatdatn2003@gmail.com>
2025-09-06 11:47:25 +0000

    path modify

--- cfg.py
+++ cfg.py
@@ -1,7 +1,9 @@
+BATCH_NAME = 'batch_2'
 
-MEDIA_DIR = '/mnt/d/AI Challenge/Data/media-info'
-DOWNLOAD_DIR = '/mnt/d/AI Challenge/Data/video'
-SCENE_DIR = '/mnt/d/AI Challenge/Data/scene'
-KEYFRAME_DIR = "/mnt/d/AI Challenge/Data/keyframes"
-FEATURE_DIR = "/mnt/d/AI Challenge/Data/features"
-ZIP_PATH = f"{FEATURE_DIR}/features.zip"
+
+MEDIA_DIR = f'/mnt/d/AI Challenge/Data/{BATCH_NAME}/media-info'
+DOWNLOAD_DIR = f'/mnt/d/AI Challenge/Data/{BATCH_NAME}/video'
+SCENE_DIR = f'/mnt/d/AI Challenge/Data/{BATCH_NAME}/scene'
+KEYFRAME_DIR = f"/mnt/d/AI Challenge/Data/{BATCH_NAME}/keyframes"
+FEATURE_DIR = f"/mnt/d/AI Challenge/Data/{BATCH_NAME}/features"
+ZIP_PATH = f"{FEATURE_DIR}/{BATCH_NAME}/features.zip"
