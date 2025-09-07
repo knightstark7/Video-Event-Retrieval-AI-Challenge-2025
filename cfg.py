617d748d723a1e488989eb95555a5905c81b5539
Khoadoan30028060 <146908560+Khoadoan30028060@users.noreply.github.com>
2025-09-07 20:23:53 +0700

    restructure folder

--- cfg.py
+++
@@ -1,9 +0,0 @@
-BATCH_NAME = 'batch_2'
-
-
-MEDIA_DIR = f'/mnt/d/AI Challenge/Data/{BATCH_NAME}/media-info'
-DOWNLOAD_DIR = f'/mnt/d/AI Challenge/Data/{BATCH_NAME}/video'
-SCENE_DIR = f'/mnt/d/AI Challenge/Data/{BATCH_NAME}/scene'
-KEYFRAME_DIR = f"/mnt/d/AI Challenge/Data/{BATCH_NAME}/keyframes"
-FEATURE_DIR = f"/mnt/d/AI Challenge/Data/{BATCH_NAME}/features"
-ZIP_PATH = f"{FEATURE_DIR}/{BATCH_NAME}/features.zip"
